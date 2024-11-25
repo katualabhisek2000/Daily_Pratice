c8e2a7fabd7be1e4e5ce902a275a9688bff2e453
katualabhisek2000 <katualabhisek2000@gmail.com>
2024-11-25 15:13:12 +0530

    first commit

---
+++ Reactive01_Mono/src/main/java/com/reactive/ReactiveDemo01Application.java
@@ -0,0 +1,13 @@
+package com.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDemo01Application.class, args);
+	}
+
+}
